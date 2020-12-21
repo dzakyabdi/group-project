1b35f10d00db8ba322d86efa554f7254d350110c
Dzaky Abdi Al Jabbar <dzaky@ui.ac.id>
2020-12-21 16:56:16 +0700

    add model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# group-project
+## Simple Image Classification With Django + TensorFlow    
+  
+This application is Using TensorFlow 1.0 + MNIST Fashion Dataset + Django to classify simple e-commerce store items
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\Universitas Indonesia\Semester 7\PDB\Punya dipsi\django-fashion-classification\venv
+set VIRTUAL_ENV=G:\Universitas Indonesia\Semester 7\PDB\django-fashion-classification\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
