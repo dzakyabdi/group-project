e306c618060e69c40b6d0dadcb910967b36f2a54
wildan <wilyan9499@gmail.com>
2020-12-22 21:18:14 +0700

    Menambahkan API untuk Resep

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 import matplotlib
+import requests
 import numpy as np
+from django.http import JsonResponse
 from django.shortcuts import render
 from tensorflow import keras
 
@@ -13,45 +15,50 @@ model = None
 # class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 #                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-class_names = ['beef_tartare', 'chicken_curry', 'chocolate_mousse', 'french_toast',
-               'fried_rice', 'hot_dog', 'ice_cream', 'lasagna', 'oysters', 'pizza', 'takoyaki']
+class_names = ['beef tartare', 'chicken curry', 'chocolate mousse', 'french toast',
+               'fried_rice', 'hot dog', 'ice cream', 'lasagna', 'oysters', 'pizza', 'takoyaki']
 
-tokopedia = "https://www.tokopedia.com/search?st=product&q="
-bukalapak = "https://www.bukalapak.com/products?search%5Bkeywords%5D="
-shopee = "https://shopee.co.id/search?keyword="
+spoonacular_api = "https://api.spoonacular.com"
 
 
 def index(request):
     global model
     global class_names
     response = {}
-    # json_file = open('model.json', 'r')
     json_file = open('model11.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = keras.models.model_from_json(loaded_model_json)
-    # model.load_weights("model.h5")
     model.load_weights("model11.h5")
-    # model = keras.models.load_model("trainedmodel_11class.hdf5", compile=False)
-    # model_best = load_model('bestmodel_3class.hdf5', compile=False)
     return render(request, 'index.html', response)
 
 
 def result(request):
     image_name = handle_image(request.FILES.get('img'))
-    # pred = real_prediction(request.FILES.get('img'))
     pred = predict_real(image_name)
-    # plot_value_test(pred)
     maximum = np.max(pred[0])
     max_index = np.where(pred[0] == maximum)
     max_index = max_index[0][0]
     name = class_names[max_index]
-    tokopedia_result = tokopedia + name
-    bukalapak_result = bukalapak + name
-    shopee_result = shopee + name
-    response = {"prediksi_title": name, "prediksi_image": image_name, "tokopedia_url": tokopedia_result,
-                "bukalapak_url": bukalapak_result, "shopee_url": shopee_result}
+    jsonSpoonacular = requests.get(spoonacular_api + '/recipes/complexSearch?instructionsRequired=true&addRecipeInformation=true&apiKey=6a3ef2a911e74c659aee8978aa91e7dc&query=' + name)
+    hasilJson = jsonSpoonacular.json()
+    response = {"prediksi_title": name, "prediksi_image": image_name, "hasil_list": hasilJson}
+
     return render(request, 'result.html', response)
+
+
+def recipe_detail(request, nama, index):
+    jsonSpoonacular = requests.get(spoonacular_api + '/recipes/complexSearch?instructionsRequired=true&addRecipeInformation=true&apiKey=6a3ef2a911e74c659aee8978aa91e7dc&query=' + nama)
+    hasilJson = jsonSpoonacular.json()
+    for num, hasil in enumerate(hasilJson['results']):
+        if num == index:
+            hasilAkhir = hasil
+            for hasil_resep in hasilAkhir["analyzedInstructions"]:
+                    resep = hasil_resep['steps']
+
+    response = { "hasil": hasilAkhir, "resep" : resep}
+
+    return render(request, 'recipe_detail.html', response)
 
 
 def handle_image(f):
@@ -63,21 +70,6 @@ def handle_image(f):
     return f.name
 
 
-# def real_prediction(image_name):
-#     global model
-#     global class_names
-#     img = image.load_img(image_name, target_size=(28, 28))
-#     img = image.img_to_array(img)
-#     img = 255 - img
-#     img_gray = np.mean(img, axis=2)
-#     img_gray = img_gray/255.0
-#
-#     img_gray_exp = (np.expand_dims(img_gray,0))
-#     pred = model.predict(img_gray_exp)
-#
-#     return pred
-
-
 def predict_real(img_name):
     img = image.load_img(settings.STATICFILES_DIRS[0] + '/image.png', target_size=(299, 299))
     img = image.img_to_array(img)
@@ -86,23 +78,3 @@ def predict_real(img_name):
 
     pred = model.predict(img)
     return pred
-
-
-def plot_value_test(predictions_array):
-    global model
-    global class_names
-
-    fig = plt.figure(num=None, figsize=(15, 8), dpi=150, facecolor='w', edgecolor='k')
-    plt.grid(False)
-    # plt.xticks(range(10))
-    plt.xticks(range(45))
-    plt.yticks([])
-    # thisplot = plt.bar(range(10), predictions_array, color="#777777")
-    thisplot = plt.bar(range(45), predictions_array[0], color="#777777")
-    plt.ylim([0, 1])
-    # predicted_label = np.argmax(predictions_array)
-    predicted_label = np.argmax(predictions_array[0])
-    thisplot[predicted_label].set_color('blue')
-    plt.xlabel("Prediction percentage")
-
-    fig.savefig(settings.STATICFILES_DIRS[0] + '/' + 'plot.png')
